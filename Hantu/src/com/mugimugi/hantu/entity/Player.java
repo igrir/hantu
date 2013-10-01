0ba6126999de9a46400458f0ca3f42915fa7cc01
gripp <oberon4mine@yahoo.com>
2013-10-01 22:05:11 +0700

    menambah player

---
+++ Hantu/src/com/mugimugi/hantu/entity/Player.java
@@ -0,0 +1,39 @@
+package com.mugimugi.hantu.entity;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Player extends Sprite {
+	
+	private int speed;
+	private int stress;
+	private int atk;
+	
+	public Player(TextureRegion textureRegion) {
+		super(textureRegion);
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getStress() {
+		return stress;
+	}
+
+	public void setStress(int stress) {
+		this.stress = stress;
+	}
+
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+}
